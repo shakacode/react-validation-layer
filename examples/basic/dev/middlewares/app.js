08a76d63673014cf4b4ed3c9c272f1de723d2952
Alex Fedoseev <alex.fedoseev@gmail.com>
2017-02-07 20:39:37 +0800

    Remove obsolete examples

--- examples/basic/dev/middlewares/app.js
+++
@@ -1,7 +0,0 @@
-const url = '/';
-
-const middleware = (req, res) => {
-  res.sendFile(`${process.cwd()}/dev/markup/index.html`);
-};
-
-export default [url, middleware];
--- examples/dependent-fields/app/validators/index.js
+++
@@ -1,3 +0,0 @@
-export { email } from './email';
-export { password } from './password';
-export { passwordConfirmation } from './passwordConfirmation';
