21aecf9ddfad958136a993c2502f07f2738115d0
Alex Fedoseev <alex.fedoseev@gmail.com>
2017-04-07 18:26:41 +0800

    Add tests for getProp

---
+++ __tests__/tests/modules/utils/getProp.spec.js
@@ -0,0 +1,34 @@
+/* @flow */
+
+import { Record } from 'immutable';
+
+import * as utils from '../../../../src/modules/utils';
+
+describe('utils.getProp()', () => {
+  it('gets prop of flat vanilla JS Object', () => {
+    const data = { attr: 1 };
+    const keyPath = 'attr';
+    const value = utils.getProp(data, keyPath);
+
+    expect(value).toBe(1);
+  });
+
+
+  it('gets prop of nested vanilla JS Object', () => {
+    const data = { nested: { attr: 1 } };
+    const keyPath = ['nested', 'attr'];
+    const value = utils.getProp(data, keyPath);
+
+    expect(value).toBe(1);
+  });
+
+
+  it('gets prop of Immutable Record', () => {
+    const Data = Record({ attr: null });
+    const data = new Data({ attr: 1 });
+    const keyPath = 'attr';
+    const value = utils.getProp(data, keyPath);
+
+    expect(value).toBe(1);
+  });
+});
--- __tests__/tests/modules/utils/x.getProp.spec.js
+++
@@ -1,7 +0,0 @@
-/* @flow */
-
-// import * as utils from '../../../../src/modules/utils';
-
-describe('utils.getProp()', () => {
-  xit('add tests', () => {});
-});
