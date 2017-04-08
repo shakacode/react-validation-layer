b8546eba7411cdad2f90b91b406d5c26e0fe1f08
Alex Fedoseev <alex.fedoseev@gmail.com>
2017-04-08 14:42:27 +0800

    Use rollup for builds

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+/* @flow */
+
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+const { BUILD_TARGET } = process.env;
+
+if (!BUILD_TARGET) {
+  throw new Error('Specify build target via BUILD_TARGET environment variable.');
+}
+
+export default {
+  entry: 'src/index.js',
+  format: BUILD_TARGET,
+  dest: `lib/react-validation-layer.${BUILD_TARGET}.js`,
+  moduleName: 'ReactValidationLayer',
+  external: ['react'],
+  globals: { react: 'React' },
+  plugins: [
+    babel({ exclude: 'node_modules/**' }),
+    resolve(),
+    commonjs(),
+  ],
+};
